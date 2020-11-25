fd21f52b92462390ec18814f2de50a142006e79b
Hsiao <rehsiao@gmail.com>
2020-11-25 15:08:41 +0800

    first commit

---
+++ myVday/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myVday
+//
+//  Created by H.W. Hsiao on 2020/11/25.
+//  Copyright © 2020 H.W. Hsiao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
