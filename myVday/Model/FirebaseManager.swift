ac97674c61c2c3b45457394b9a4d335807ff6b82
Hsiao <rehsiao@gmail.com>
2020-11-30 18:02:29 +0800

    fetch restaurant basic info from firebase, add marker of restaurant on map

--- myVday/Model/FirebaseManager.swift
+++ myVday/Model/FirebaseManager.swift
@@ -10,13 +10,53 @@ import Foundation
 import FirebaseFirestore
 
 protocol FirebaseManagerDelegate: AnyObject {
-    func didDownloadData()
+    func fireManager(_ manager: FirebaseManager, didDownload basicData: [QueryDocumentSnapshot])
+    func fireManager(_ manager: FirebaseManager, didDownload detailData: [QueryDocumentSnapshot], type: DataType)
+}
+
+enum DataType: String, CaseIterable {
+    case comments = "comments"
+    case hashtags = "hashtags"
+    case hours = "hours"
 }
 
 class FirebaseManager {
     let fireDB = Firestore.firestore()
+    let dataType = DataType.allCases
+    var restaurantId = [String]()
     weak var delegate: FirebaseManagerDelegate?
     
-    //fetch data from firebase
+    //fetch basic data of restaurant from firebase
+    func fetchData() {
+        fireDB.collection("Restaurant").getDocuments { (snapshot, error) in
+            if let err = error {
+                print("Error getting docs: \(err)")
+            } else {
+                if let docArray = snapshot?.documents {
+                    for document in docArray {
+                        let id = document.documentID
+                        self.restaurantId.append(id)
+                    }
+                    for type in self.dataType {
+                        self.fetchSubCollections(type: type)
+                    }
+                    self.delegate?.fireManager(self, didDownload: docArray)
+                }
+            }
+        }
+    }
     
+    func fetchSubCollections(type: DataType) {
+        for rstId in restaurantId {
+            fireDB.collection("Restaurant").document(rstId).collection(type.rawValue).getDocuments { (snapshot, error) in
+                if let err = error {
+                    print("Error getting docs: \(err)")
+                } else {
+                    if let docArray = snapshot?.documents {
+                        self.delegate?.fireManager(self, didDownload: docArray, type: type)
+                    }
+                }
+            }
+        }
+    }
 }
